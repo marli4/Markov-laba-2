6705e3eb5f1f2865f72f743ab92a76fc48e9d86b
cattheri <catherinerigunova@yandex.ru>
2025-05-21 10:50:31 +0400

    pick restaurant

---
+++ Markov-laba-2/Controllers/RestaurantPageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Markov_laba_2.Controllers
+{
+    public class RestaurantPageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
